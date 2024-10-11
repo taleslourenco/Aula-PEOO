7595a2baf921a478ad8b2d7a2fa82ba88b646b54
Tales Lourenço <161078229+taleslourenco@users.noreply.github.com>
2024-10-11 20:51:59 +0000

    update

--- Steamlit/RetanguloUI.py
+++ Steamlit/RetanguloUI.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from retangulo import Retangulo
+from Retangulo import Retangulo
 
 class RetanguloUI:
     def main():
--- Steamlit/index.py
+++ Steamlit/index.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from retanguloUI import RetanguloUI
+from RetanguloUI import RetanguloUI
 
 RetanguloUI.main()
 
