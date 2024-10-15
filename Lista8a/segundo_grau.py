c713a187b54226b375e603330666c8b45da57846
Tales Lourenço <161078229+taleslourenco@users.noreply.github.com>
2024-10-15 18:51:46 +0000

    a

--- Lista8a/segundo_grau.py
+++ Lista8a/segundo_grau.py
@@ -13,11 +13,6 @@ class Bhaskara:
         return self.__b ** 2 - 4 * self.__a * self.__c
     
     def calc_raizes(self):
-        if Bhaskara.calc_delta() < 0:
-            return "Deu ruim visse"
-        elif Bhaskara.calc_delta() == 0:
-            return 
-        else:
-            x1 = ((self.__b * -1) + math.sqrt(Bhaskara.calc_delta())) / (2*self.__a)
-            x2 = ((self.__b * -1) - math.sqrt(Bhaskara.calc_delta())) / (2*self.__a)
-            return x1, x2
+        x1 = ((self.__b * -1) + math.sqrt(Bhaskara.calc_delta())) / (2*self.__a)
+        x2 = ((self.__b * -1) - math.sqrt(Bhaskara.calc_delta())) / (2*self.__a)
+        return x1, x2
