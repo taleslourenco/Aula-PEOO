437295af485bb2d482b39462cadf9cadff181a2a
Tales Lourenço <161078229+taleslourenco@users.noreply.github.com>
2024-10-15 19:01:54 +0000

    a

--- Lista8a/segundo_grau_UI.py
+++ Lista8a/segundo_grau_UI.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 from segundo_grau import Bhaskara
 
 class segundograuUI:
@@ -13,4 +14,27 @@ class segundograuUI:
             st.write(b)
             st.write(f"Delta = {b.calc_delta()}")
             st.write(f"Raizes (x1, x2) = {b.calc_raizes()}")
+
+            xmin = 5
+            xmax = 15
+            n = 150
+            d = (xmax - xmin)/n  # 0.5
+            px = []
+            py = []
+            x = xmin
+            while x < xmax:
+                y = x**2 - 5*x + 6
+                px.append(x)
+                py.append(y)
+                x = x + d
+            x = xmax
+            y = x**2 - 5*x + 6
+            px.append(x)
+            py.append(y)
+
+            dic = { "x" : px, "y" : py }
+            chart_data = pd.DataFrame(dic)
+            st.line_chart(chart_data, x = "x", y = "y") 
+       
+
         
