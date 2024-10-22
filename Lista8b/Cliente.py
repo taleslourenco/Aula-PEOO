bbb90c1642b9d621d5cdd4b09a751957a5294007
Tales Lourenço <161078229+taleslourenco@users.noreply.github.com>
2024-10-22 13:01:48 +0000

    a

--- Lista8b/Cliente.py
+++ Lista8b/Cliente.py
@@ -1,9 +1,9 @@
 class Cliente:
     def __init__(self, id:int, nome:str, email:str, fone:str):
-        self.__id = id
-        self.__nome = nome
-        self.__email = email
-        self.__fone = fone
+        self.id = id
+        self.nome = nome
+        self.email = email
+        self.fone = fone
     
     def __str__(self):
-        return f"id = {self.__id} | nome = {self.__nome} | email = {self.__email} | fone = {self.__fone}"
+        return f"id = {self.id} | nome = {self.nome} | email = {self.email} | fone = {self.fone}"
