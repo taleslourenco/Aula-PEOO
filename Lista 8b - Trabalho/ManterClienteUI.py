90b90700d3cfcd623bacdea7b24e0062e1dfad99
Tales Lourenço <161078229+taleslourenco@users.noreply.github.com>
2024-10-22 12:45:50 +0000

    trabalho

--- Lista 8b - Trabalho/ManterClienteUI.py
+++ Lista 8b - Trabalho/ManterClienteUI.py
@@ -26,7 +26,7 @@ class ClienteUI:
             View.cliente_inserir(nome, email, fone)
 
     def Atualizar():
-        id = st.selectbox("Atualização de clientes", View.cliente_listar())
+        id = st.selectbox("Atualização de Clientes", View.cliente_listar())
         nome = st.text_input("Informe o novo nome")
         email = st.text_input("Informe o novo e-mail")
         fone = st.text_input("Informe o novo fone")
@@ -35,6 +35,6 @@ class ClienteUI:
             View.cliente_atualizar(id, nome, email, fone)
     
     def Excluir():
-
+        id = st.selectbox("Exclusão de Clientes", View.cliente_listar())
         if st.button("Excluir"):
-            View.cliente_excluir()
+            View.cliente_excluir(id)
