8c3142c4049a31c33f819d3b65d90b929c7229f6
Tales Lourenço <161078229+taleslourenco@users.noreply.github.com>
2024-10-22 14:03:03 +0000

    a

--- Lista8b/ManterClienteUI.py
+++ Lista8b/ManterClienteUI.py
@@ -29,9 +29,10 @@ class ClienteUI:
 
     def Atualizar():
         clientes = View.cliente_listar()
+        for c in clientes:
+            ids = c.id
         
-
-        id = st.selectbox("Atualização de Clientes", View.cliente_listar(), index = 0)
+        id = st.selectbox("Atualização de Clientes", View.cliente_listar(), index = ids)
         nome = st.text_input("Informe o novo nome")
         email = st.text_input("Informe o novo e-mail")
         fone = st.text_input("Informe o novo fone")
@@ -40,6 +41,10 @@ class ClienteUI:
             View.cliente_atualizar(id, nome, email, fone)
     
     def Excluir():
-        id = st.selectbox("Exclusão de Clientes", View.cliente_listar(), index = 0)
+        clientes = View.cliente_listar()
+        for c in clientes:
+            ids = c.id
+            
+        id = st.selectbox("Exclusão de Clientes", View.cliente_listar(), index = ids)
         if st.button("Excluir"):
             View.cliente_excluir(id)
