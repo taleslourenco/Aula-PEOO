8efbe37c45ba74b41b45f9cb5a5d502cb470a27a
Tales Lourenço <161078229+taleslourenco@users.noreply.github.com>
2024-10-28 22:27:11 +0000

    a

--- Lista8c/Templates/Index.py
+++ Lista8c/Templates/Index.py
@@ -4,6 +4,8 @@ class IndexUI:
     def main():
         st.sidebar.title("Menu")
         opcao = st.sidebar.selectbox("Selecione uma opção", ["Cadastro de Clientes", "Cadastro de Horários", "Cadastro de Serviços", "Abrir Agenda do Dia"])
+        if opcao("Cadastro de Clientes"):
+            ManterClienteUI.ManterClienteUI.main()
         
         
 IndexUI.main()
