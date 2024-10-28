fb6a38bab883d9993a37e342eaec87bb738f67a9
Tales Lourenço <161078229+taleslourenco@users.noreply.github.com>
2024-10-28 22:28:37 +0000

    a

--- Lista8c/Templates/Index.py
+++ Lista8c/Templates/Index.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import ManterClienteUI, ManterHorarioUI, ManterServicoUI
 
 class IndexUI:
     def main():
@@ -6,6 +7,11 @@ class IndexUI:
         opcao = st.sidebar.selectbox("Selecione uma opção", ["Cadastro de Clientes", "Cadastro de Horários", "Cadastro de Serviços", "Abrir Agenda do Dia"])
         if opcao("Cadastro de Clientes"):
             ManterClienteUI.ManterClienteUI.main()
+        if opcao("Cadastro de Horários"):
+            ManterHorarioUI.ManterHorarioUI.main()
+        if opcao("Cadastro de Serviços"):
+            ManterServicoUI.ManterServicoUI.main()
+        
         
         
 IndexUI.main()
