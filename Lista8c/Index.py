a2c5e729ba42e82daaf15e6ff9114436e52a716e
Tales Lourenço <161078229+taleslourenco@users.noreply.github.com>
2024-10-29 18:29:41 +0000

    a

--- Lista8c/Index.py
+++ Lista8c/Index.py
@@ -6,12 +6,12 @@ from Templates.manterServicoUI import ManterServicoUI
 class IndexUI:
     def main():
         st.sidebar.title("Menu")
-        opcao = st.sidebar.selectbox("Selecione uma opção", ["Cadastro de Clientes", "Cadastro de Horários", "Cadastro de Serviços", "Abrir Agenda do Dia"])
-        if opcao(["Cadastro de Clientes"]):
-            ManterClienteUI.ManterClienteUI.main()
-        if opcao(["Cadastro de Horários"]):
-            ManterHorarioUI.ManterHorarioUI.main()
-        if opcao(["Cadastro de Serviços"]):
-            ManterServicoUI.ManterServicoUI.main()
+        opcao = st.sidebar.selectbox("Selecione uma opção", ["Cadastro de Clientes", "Cadastro de Horários", "Cadastro de Serviços"])
+        if opcao == "Cadastro de Clientes":
+            ManterClienteUI.main()
+        if opcao == "Cadastro de Horários":
+            ManterHorarioUI.main()
+        if opcao == "Cadastro de Serviços":
+            ManterServicoUI.main()
         
 IndexUI.main()
