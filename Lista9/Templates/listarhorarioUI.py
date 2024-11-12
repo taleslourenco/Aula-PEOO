225d4f78d45483328b0fa1ffa980719508b3362d
Tales Lourenço <161078229+taleslourenco@users.noreply.github.com>
2024-11-12 01:40:52 +0000

    a

---
+++ Lista9/Templates/listarhorarioUI.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+from views import View
+
+class ListarHorarioUI:
+    def main():
+        st.header("Horários Disponíveis")
+        ListarHorarioUI.listar()
+
+    def listar():
+        horarios = View.horario_listar_disponiveis()
+        if len(horarios) == 0:
+            st.write("Nenhum horário disponível")
+        else:
+            dic = []
+            for obj in horarios: dic.append(obj.to_json())
+            df = pd.DataFrame(dic)
+            df_limited = df.iloc[:, :3]
+            st.dataframe(df_limited)
